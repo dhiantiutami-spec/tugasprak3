fcd9d41603335b9168544d9ca547e7ae38a56327
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-03-10 20:35:30 +0700

    memanggil prosedur input

--- file.cpp
+++ file.cpp
@@ -34,5 +34,8 @@ int main() {
     float berat, tinggi, bmi;
     string status;
     cout << "--- Selamat Datang Mahasiswa Kelas A 2025---" << endl;
+
+    // Memanggil prosedur input
+    inputData(berat, tinggi);
     
 }

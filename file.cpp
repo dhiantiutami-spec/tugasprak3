030f123bab552f44a20aa32c6c7de0ecd89c9f53
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-03-10 20:22:19 +0700

    fungsi untuk menghitung BMI

--- file.cpp
+++ file.cpp
@@ -7,5 +7,9 @@ void inputData(float &berat, float &tinggi) {
     cin >> berat;
     cout << "Masukkan tinggi badan (meter): ";
     cin >> tinggi ;
+}
 
+// Fungsi untuk menghitung BMI
+float hitungBMI(float berat, float tinggi) {
+    return berat / (tinggi * tinggi);
 }
